b7293dc1efdc0c4aa052e4a5fec861338c263fb2
hagiwaratakayuki <hagiwaratakayuki@gmail.com>
2024-09-10 03:57:02 +0900

    start trustchain

--- contracts/contracts/core/auth/DelegateValidation.sol
+++ contracts/contracts/core/auth/DelegateValidation.sol
@@ -1,78 +1,129 @@
 // SPDX-License-Identifier:AGPL-3.0
 pragma solidity ^0.8.24;
 
-string constant VALIDATER_CHANGE_VALIDATER = "validater change validater";
-string constant DEFAULT_UPDATE_VALIDATER = "default update validater";
-
 contract DelegateValidation {
-    mapping(string => address) _defaultOperationPermission;
+    constructor(address operationValidater) {
+        _operationValidater = operationValidater;
+    }
 
+    /** default operation  */
+    address _operationValidater;
+
+    function updateOperationValidater(
+        address operationValidater
+    ) external _checkOperationPermission {
+        _operationValidater = operationValidater;
+    }
+
+    modifier _checkOperationPermission() {
+        require(_operationValidater == msg.sender, "not permited");
+        _;
+    }
+    /**
+     * default permissions
+     */
+    address defaultOperationValidater;
+
+    function _checkDefaultOperationValidater() public view returns (bool) {
+        return msg.sender == defaultOperationValidater;
+    }
+
+    /** default permssion  */
     mapping(string => mapping(address => bool)) _defaultPermission;
-    mapping(address => mapping(string => mapping(address => uint8))) _addressPermission; // 0 unset ,1 permit 2 denay
-
-    constructor(
-        address validaterChangeValidater,
-        address defaultUpdateValidater
-    ) {
-        _defaultOperationPermission[
-            VALIDATER_CHANGE_VALIDATER
-        ] = validaterChangeValidater;
-        _defaultOperationPermission[
-            DEFAULT_UPDATE_VALIDATER
-        ] = defaultUpdateValidater;
-    }
-
-    function updateValidaterChangeValidater(address validater) public {
-        require(
-            _defaultOperationPermission[VALIDATER_CHANGE_VALIDATER] ==
-                msg.sender,
-            "forbidden"
-        );
-        _defaultOperationPermission[VALIDATER_CHANGE_VALIDATER] = validater;
-    }
-
-    function updateDefaultUpdateValidater(address validater) public {
-        require(
-            _defaultOperationPermission[VALIDATER_CHANGE_VALIDATER] ==
-                msg.sender,
-            "forbidden"
-        );
-        _defaultOperationPermission[DEFAULT_UPDATE_VALIDATER] = validater;
-    }
 
     function _updateDefaultPermission(
         string memory key,
         address validater,
         bool permission
-    ) private {
-        require(
-            _defaultOperationPermission[DEFAULT_UPDATE_VALIDATER] == msg.sender,
-            "not permited"
-        );
+    ) external _checkOperationPermission {
         _defaultPermission[key][validater] = permission;
     }
 
-    function _checkAccountPermssion(
-        string memory key,
-        address target
-    ) private view returns (bool) {
-        return _defaultPermission[key][target] == true;
+    function _checkDefaultPermssion(
+        string memory key
+    ) public view returns (bool) {
+        return _defaultPermission[key][msg.sender];
     }
 
-    function _updateAccountPermssion(
+    /**
+     * Account permission
+     *
+     *
+     */
+
+    // account operation
+
+    mapping(address => mapping(address => uint8)) _accountOperationValidaters; // 0 undefined 1 allow 2 denay
+
+    function updateAccountOperationValidater(
+        address account,
+        address operationValidater
+    ) external _checkAccountOperationPermission(account) {
+        _operationValidater = operationValidater;
+    }
+
+    modifier _checkAccountOperationPermission(address account) {
+        uint8 accountPermssion = _accountOperationValidaters[account][
+            msg.sender
+        ];
+        require(accountPermssion != 2, "validater denayed");
+
+        require(
+            defaultOperationValidater == msg.sender || accountPermssion == 1,
+            "not permited"
+        );
+        _;
+    }
+
+    mapping(address => mapping(string => mapping(address => uint8))) _acountPermission; // 0 unset ,1 permit 2 denay
+
+    modifier delegateValidate(address account, string memory key) {
+        require(_checkPermission(account, key), "invalid validater");
+        _;
+    }
+
+    function _updateAccountPermission(
         string memory key,
         address account,
-        address target,
+        address validater,
         uint8 permission
-    ) private {
-        require(permission == 1 || permission == 2, "invalid");
-        _addressPermission[account][key][target] = permission;
+    ) external _checkAccountOperationPermission(account) {
+        require(
+            permission == 1 || permission == 2,
+            "invalid permsion valiue. 1 is allow 2 is denay"
+        );
+        _acountPermission[account][key][validater] = permission;
     }
 
     function _checkAccountPermission(
         address account,
         string memory key
-    ) private view returns (bool) {
-        return _addressPermission[account][key][msg.sender] == 1;
+    ) public view returns (bool, bool) {
+        uint8 permission = _acountPermission[account][key][msg.sender];
+        bool allow = permission == 1;
+        bool denay = permission == 2;
+        return (allow, denay);
+    }
+
+    modifier isPermited(address account, string memory key) {
+        require(_checkPermission(account, key), "permision denayed");
+        _;
+    }
+
+    function _checkPermission(
+        address account,
+        string memory key
+    ) public view returns (bool) {
+        bool allow;
+        bool denay;
+
+        (allow, denay) = _checkAccountPermission(account, key);
+        if (allow == true) {
+            return true;
+        }
+        if (denay == true) {
+            return false;
+        }
+        return _checkDefaultPermssion(key);
     }
 }
--- contracts/contracts/core/storage/SignStorage.sol
+++ contracts/contracts/core/storage/SignStorage.sol
@@ -1,55 +1,95 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.24;
 
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
-import "@openzeppelin/contracts/utils/cryptography/MessageHashUtils.sol";
-import "hardhat/console.sol";
+
+//import "hardhat/console.sol";
 import {DelegateValidation} from "../auth/DelegateValidation.sol";
+import {StrSlice, toSlice} from "@dk1a/solidity-stringutils/src/StrSlice.sol";
+import {Strings} from "@openzeppelin/contracts/utils/Strings.sol";
+using Strings for address;
 
-// update paycheck sign
+// authorise keyword update
+
+string constant PAYMENT = "payment";
+string constant SIGN = "sign";
+
+//delimiter
+string constant _ADDRESS_URI_DELIMITER = unicode"_";
+
 contract SignStorage is DelegateValidation {
-    using ECDSA for bytes32;
-
-    constructor(address defaultOperater) DelegateValidation(defaultOperater) {}
-
-    struct Signer {
-        address signAccount;
+    struct TrustChain {
+        uint length;
+        mapping(uint => address) iterater;
+        mapping(address => bool) index;   
     }
-    struct SignOld {
-        bytes acceptSignature; // ECDSA sign for target account
-        bytes requstSignature;
-        address signer;
-        address truster;
-        address payChecker;
-        uint fee; //wei
+    struct  Trust{
+        TrustChain chain;
+        string format;
+        bytes sign;        
     }
 
-    uint payCheckerCount;
-
-    struct Sign {
-        string data;
-        bytes signedMesage;
-        bytes signatuter;
-    }
-    struct Signs {
+    struct Declare {
         string format;
         string evidence;
-        mapping(address => Sign) signs;
+        bytes signature;
     }
-    mapping(uint => Signs) signsMapL;
-    uint signsCount;
 
-    function insertOrUpdateSigner(
-        address signAccount,
-        string memory callMethod,
-        address callAccount
+    constructor(
+        address validaterChangeValidater,
+        address defaultUpdateValidater,
+        address paymentValidater,
+        address signValidater
+    ) DelegateValidation(validaterChangeValidater, defaultUpdateValidater) {
+        
+        _updateDefaultPermission(PAYMENT, paymentValidater, true);
+        _updateDefaultPermission
+    }
+
+   
+
+    mapping(uint => Declare) declareMapL;
+    uint declaresCount;
+
+    mapping(string => uint) declareIndex;
+
+    function sign(
+        string memory uri,
+        string memory format,
+        string memory evidence,
+        bytes memory signature,
+        address[] memory singners
     ) public {
-        require(
-            _signerUpdateAuthorityContracts[msg.sender],
-            "authoraization failed"
-        );
-        signerMapping[tx.origin].signAccount = signAccount;
-        signerMapping[tx.origin].callAccount = callAccount;
-        signerMapping[tx.origin].callMethod = callMethod;
+        bool isValid;
+        isValid = super._checkDefaultPermssion(SIGN);
+        if (isValid == false) {
+            for (uint i = 0; i < singners.length; ) {
+                address signer;
+                signer = singners[i];
+                isValid =
+                    isValid ||
+                    super._checkAccountPermission(signer, SIGN);
+                if (isValid == true) {
+                    break;
+                }
+                unchecked {
+                    i++;
+                }
+            }
+        }
+        require(isValid, "vealidater does not authoraised");
+
+        declareMapL[declaresCount] = Declare(format, evidence, signature);
+        StrSlice uriSlice = toSlice(uri);
+        for (uint i = 0; i < singners.length; i++) {
+            address signer;
+            signer = singners[i];
+            StrSlice[] memory slices = new StrSlice[](2);
+            string memory key;
+            slices[0] = toSlice(signer.toHexString());
+            slices[1] = uriSlice;
+            key = toSlice(_ADDRESS_URI_DELIMITER).join(slices);
+            declareIndex[key] = declaresCount;
+        }
+        declaresCount++;
     }
 }
